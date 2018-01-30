b525d1c0729e56a3ec958011f7e460e140ccee89
Amit Kumar Gupta <theamit97@gmail.com>
2018-01-30 23:53:01 +0530

    Designing basic windows

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets
-from ui_definitions import Ui_GeneratorMainWindow
+from ui_definitions import Ui_GeneratorMainWindow, Ui_GeneratorWelcomeDialog
 import sys
 
 
@@ -10,10 +10,17 @@ class GeneratorMainWindow(QtWidgets.QMainWindow, Ui_GeneratorMainWindow):
         self.setupUi(self)
 
 
+class GeneratorWelcomeWidget(QtWidgets.QWidget, Ui_GeneratorWelcomeDialog):
+
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.setupUi(self)
+
+
 def main():
     print("Starting the question paper generator....")
     app = QtWidgets.QApplication(sys.argv)
-    generator_main_window = GeneratorMainWindow()
+    generator_main_window = GeneratorWelcomeWidget()
     generator_main_window.show()
     app.exec_()
 
