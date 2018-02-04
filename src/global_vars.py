c937eaa69dc1bf6e5f5f03d986c32167d6102d32
Amit Kumar <theamit97@gmail.com>
2018-02-04 19:10:28 +0530

    Implementing QP Editor

--- src/global_vars.py
+++ src/global_vars.py
@@ -2,6 +2,6 @@ class GlobalVars:
 
     def __init__(self):
         self.default_qp_values = dict(exam_name='Exam Name', course_code='0001', qp_code='12345', program='B.E',
-                                      max_marks=100, date='12/12/2017', semester='3', duration=90, num_parts=1,
+                                      max_marks=100, date='12122017', semester=3, duration=90, num_parts=1,
                                       qp_data='')
 
--- src/main_window.py
+++ src/main_window.py
@@ -1,6 +1,7 @@
 from ui_definitions import Ui_GeneratorMainWindow
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from helper_widgets import NewQPWidget, OpenQPWidget
+from PyQt5.QtCore import QDate
 import json
 
 
@@ -15,13 +16,29 @@ class GeneratorMainWindow(QtWidgets.QMainWindow, Ui_GeneratorMainWindow):
         # Connect actions to slots
         self.actionNew_QP.triggered.connect(self.create_file_action)
         self.actionLoad_QP.triggered.connect(self.open_file_action)
+        self.pushButton.clicked.connect(self.generate_qp)
+        self.qp_data_tool_button.clicked.connect(self.load_qp_data_tool_clicked)
 
-        # Slots:
+    # Slots:
+
+    def load_qp_data_tool_clicked(self):
+        self.qp_data_file = QtWidgets.QFileDialog.getOpenFileName(self, 'Open Question paper data', str(QtCore.QStandardPaths.DocumentsLocation),
+                                              'Question Paper Data (*.qpd)')[0]
+        if not self.qp_data_file:
+            return
+        self.qp_data_line_edit.setText(self.qp_data_file)
+
     def create_file_action(self):
         create_file_dialog = NewQPWidget()
         if create_file_dialog.exec_() == 0:
             return
         self.load_qp_data(create_file_dialog.new_file)
+
+    def generate_qp(self):
+        if not self.qp_data_line_edit.text():
+            QtWidgets.QMessageBox.critical(self, 'Error', 'Question Bank data must be provided.',
+                                           QtWidgets.QMessageBox.Ok)
+            return
 
     def open_file_action(self):
         open_file_dialog = OpenQPWidget()
@@ -49,4 +66,8 @@ class GeneratorMainWindow(QtWidgets.QMainWindow, Ui_GeneratorMainWindow):
         self.program_line_edit.setText(json_data['program'])
         self.course_code_line_edit.setText(json_data['course_code'])
         self.marks_spin_box.setValue(json_data['max_marks'])
-        self.exam_date_edit.setDate()
+        self.exam_date_edit.setDate(QDate.fromString(json_data['date'], 'ddMMyyyy'))
+        self.sem_spin_box.setValue(json_data['semester'])
+        self.duration_spin_box.setValue(json_data['duration'])
+        self.num_parts_spin_box.setValue(json_data['num_parts'])
+        self.qp_data_line_edit.setText(json_data['qp_data'])
