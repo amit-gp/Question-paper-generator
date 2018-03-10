d7dbd1a554670bf697b26154da19beefbf20fca1
Amit Kumar Gupta <theamit97@gmail.com>
2018-03-10 16:53:42 +0530

    Implementing saving.

--- src/main_window.py
+++ src/main_window.py
@@ -13,9 +13,12 @@ class GeneratorMainWindow(QtWidgets.QMainWindow, Ui_GeneratorMainWindow):
         self.setupUi(self)
         if not self.load_qp_data(file_path):
             return
+
+        self.file_path = file_path
         # Connect actions to slots
         self.actionNew_QP.triggered.connect(self.create_file_action)
         self.actionLoad_QP.triggered.connect(self.open_file_action)
+        self.actionSave_QP.triggered.connect(self.save_file_action)
         self.pushButton.clicked.connect(self.generate_qp)
         self.qp_data_tool_button.clicked.connect(self.load_qp_data_tool_clicked)
         self.qpd_open_push_button.clicked.connect(self.qpd_open_clicked)
@@ -23,15 +26,35 @@ class GeneratorMainWindow(QtWidgets.QMainWindow, Ui_GeneratorMainWindow):
     # Slots:
 
     def qpd_open_clicked(self):
-
         self.qpdwelcome = QPDWelcomeDialog()
         self.qpdwelcome.show()
 
     def load_qp_data_tool_clicked(self):
-        self.qp_data_file = QtWidgets.QFileDialog.getOpenFileName(self, 'Open Question paper data', str(QtCore.QStandardPaths.DocumentsLocation),
-                                              'Question Paper Data (*.qpd)')[0]
+        self.qp_data_file = QtWidgets.QFileDialog.getOpenFileName(self, 'Open Question paper data',
+                                                                  str(QtCore.QStandardPaths.DocumentsLocation),
+                                                                        'Question Paper Data (*.qpd)')[0]
         if not self.qp_data_file:
             return
+        self.qp_data_line_edit.setText(self.qp_data_file)
+
+    def save_file_action(self):
+        sv_file = open(self.file_path, 'w')
+        json.dump(self.get_json_data_from_fields(), sv_file, ensure_ascii=False)
+        print("\nSaving file to: %s", self.file_path)
+        self.statusbar.showMessage("Saved file to :" + self.file_path, 3000)
+
+    def get_json_data_from_fields(self):
+        json_dict = dict(exam_name=self.exam_name_line_edit.text(),
+                         course_code=self.course_code_line_edit.text(),
+                         qp_code=self.qp_code_line_edit.text(),
+                         program=self.program_line_edit.text(),
+                        max_marks=self.marks_spin_box.value(),
+                         date=QDate.toString(self.exam_date_edit.date(), 'ddMMyyyy'),
+                         semester=self.sem_spin_box.value(),
+                         duration=self.duration_spin_box.value(),
+                         num_parts=self.num_parts_spin_box.value(),
+                         qp_data=self.qp_data_line_edit.text())
+        return json_dict
 
     def create_file_action(self):
         create_file_dialog = NewQPWidget()
@@ -44,8 +67,6 @@ class GeneratorMainWindow(QtWidgets.QMainWindow, Ui_GeneratorMainWindow):
             QtWidgets.QMessageBox.critical(self, 'Error', 'Question Bank data must be provided.',
                                            QtWidgets.QMessageBox.Ok)
             return
-
-        self.new_mw_test = GeneratorMainWindow()
 
     def open_file_action(self):
         open_file_dialog = OpenQPWidget()
